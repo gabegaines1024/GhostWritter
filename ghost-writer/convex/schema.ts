679e8f607b713422c098d5b119b2b117cb76db6c
Gabe <gabegaines1024@gmail.com>
2025-12-23 18:55:34 -0600

    even better table

--- ghost-writer/convex/schema.ts
+++ ghost-writer/convex/schema.ts
@@ -1,12 +1,26 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
-import { title } from "process";
+import { permission, title } from "process";
 
 export default defineSchema({
-  users: defineTable({
+  scripts: defineTable({
     title: v.string(),
-    author: v.string(),
+    author: v.string(), 
     createdAt: v.number(),
     updatedAt: v.number(),
   }),
+  scriptBlocks: defineTable({
+    scriptId: v.id("scripts"),
+    blockId: v.string(),
+    type: v.string(),
+    content: v.string(),
+    order: v.number()
+  }),
+  collaborators: defineTable({
+    scriptId: v.id("scripts"),
+    userId: v.string(),
+    permission: v.union(v.literal("read"), v.literal("write"), v.literal("admin")),
+    createdAt: v.number(),
+    updatedAt: v.number(),
+  })
 });
